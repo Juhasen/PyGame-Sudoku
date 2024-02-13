40d9a9e4a7acf4fdc327ed2eeeb1aa2846e42ed5
Juhasen <juszczykkrystian2003@gmail.com>
2024-02-14 00:44:09 +0100

    TODO: FIX THE DISPLAY OF NUMBERS

--- game.py
+++ game.py
@@ -1,10 +1,11 @@
 import pygame
 import sys
 
-from render import render_board, render_numbers, generate_numbers
-from mouse_action import track_mouse
+from render import render_board, render_numbers, generate_numbers, render_description, clear_screen
+from mouse_action import handle_click
+from validate import solve_sudoku
 
-WIDTH = 750
+WIDTH = 850
 HEIGHT = 550
 
 if __name__ == "__main__":
@@ -25,23 +26,51 @@ if __name__ == "__main__":
     screen.fill((211, 211, 211))
 
     board = [[0 for i in range(9)] for j in range(9)]
-
+    generated_cells = []
     render_board(screen)
-    generate_numbers(screen, board)
+    generate_numbers(board, generated_cells)
+    render_description(screen)
+    pos = []
+    row, col = 0, 0
 
     while running:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+                board = [[0 for i in range(9)] for j in range(9)]
+                clear_screen(screen)
+                generate_numbers(board, generated_cells)
+                break
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = handle_click(screen, board)
+                if pos:
+                    row, col = pos
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
                 running = False
+            elif event.type == pygame.KEYDOWN and pos and pos not in generated_cells:
+                if event.key == pygame.K_1:
+                    board[row][col] = 1
+                elif event.key == pygame.K_2:
+                    board[row][col] = 2
+                elif event.key == pygame.K_3:
+                    board[row][col] = 3
+                elif event.key == pygame.K_4:
+                    board[row][col] = 4
+                elif event.key == pygame.K_5:
+                    board[row][col] = 5
+                elif event.key == pygame.K_6:
+                    board[row][col] = 6
+                elif event.key == pygame.K_7:
+                    board[row][col] = 7
+                elif event.key == pygame.K_8:
+                    board[row][col] = 8
+                elif event.key == pygame.K_9:
+                    board[row][col] = 9
+                elif event.key == pygame.K_d:
+                    board[row][col] = 0
+                elif event.key == pygame.K_s:
+                    solve_sudoku(board)
 
-        if pygame.mouse.get_pressed()[0]:
-            pos = track_mouse(screen)
-
-            font = pygame.font.Font(None, 36)
-            text = font.render(str(pos), True, (0, 0, 0))
-            screen.blit(text, (550, 300))
-
-        render_numbers(screen, board)
+        render_numbers(screen, board, generated_cells)
 
         pygame.display.update()
         clock.tick(60)
