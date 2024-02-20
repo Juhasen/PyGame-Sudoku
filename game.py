164e7099f20d9e74890f95c6163e6d7c48f05631
Juhasen <juszczykkrystian2003@gmail.com>
2024-02-20 19:38:07 +0100

    Fixed solving the board and updated the WIN state

--- game.py
+++ game.py
@@ -1,11 +1,11 @@
 import pygame
 import sys
 
-from render import draw_board, draw_numbers, generate_numbers, draw_description, clear_screen
+from render import draw_board, draw_numbers, generate_numbers, draw_description, clear_screen, draw_win
 from mouse_action import handle_click
 from validate import solve_sudoku
 
-WIDTH = 850
+WIDTH = 880
 HEIGHT = 550
 
 if __name__ == "__main__":
@@ -30,28 +30,41 @@ if __name__ == "__main__":
     draw_description(screen)
     pos = []
     row, col = 0, 0
+    board_solved = False
 
     while running:
         for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
+                running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_r:
                     clear_screen(screen)
                     board = generate_numbers(generated_cells)
+                    board_solved = False
                     break
-                elif event.key == pygame.K_s:
+                elif event.key == pygame.K_s and not board_solved:
+                    old_board = [row[:] for row in board]
                     for i in range(9):
                         for j in range(9):
-                            if (i, j) not in generated_cells:
+                            if (i, j) in generated_cells:
                                 board[i][j] = 0
                     solve_sudoku(board)
+                    how_many_errors = 0
+                    wrong_cells = []
+                    for i in range(9):
+                        for j in range(9):
+                            if old_board[i][j] != board[i][j]:
+                                wrong_cells.append((i, j))
+                                how_many_errors += 1
+                    draw_win(screen, how_many_errors, wrong_cells)
+                    board_solved = True
                     break
-            if event.type == pygame.MOUSEBUTTONDOWN:
+
+            if event.type == pygame.MOUSEBUTTONDOWN and not board_solved:
                 pos = handle_click(screen, board)
                 if pos:
                     row, col = pos
-            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
-                running = False
-            elif event.type == pygame.KEYDOWN and pos and pos in generated_cells:
+            elif event.type == pygame.KEYDOWN and pos and pos in generated_cells and not board_solved:
                 if event.key == pygame.K_1:
                     board[row][col] = 1
                 elif event.key == pygame.K_2:
